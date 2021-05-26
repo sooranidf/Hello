de0264100411230fa2eebe85bce946ede6348812
Daniel <84752346+sooranidf@users.noreply.github.com>
2021-05-26 18:54:44 -0400

    Add files via upload
    Added personal information

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/**********************************************************************
+ *Hello
+***********************************************************************
+* Daniel Soorani
+* 05/24/2021
+* CMSC 255 SU2021
+***********************************************************************/
+
+public class Hello {
+	
+		public static void main(String[] args){
+	System.out.println("Daniel Soorani");
+	System.out.println("Hello");
+	System.out.println("Says hello and gives details to user");
+	System.out.println("05/25/2021");
+	System.out.println("<Cmsc255sum");
+	System.out.println();
+	}
+}
